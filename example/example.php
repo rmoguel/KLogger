5e823c687bc98a76ee302a459388c5c00c849417
Toomas Römer <toomasr@gmail.com>
2012-07-16 19:04:15 +0300

    Rename

--- example/example.php
+++ example/example.php
@@ -1,20 +1,16 @@
 <?php
 
 # Should log to the same directory as this file
-require dirname(__FILE__) . '/../src/KLogger.php';
+require dirname(__FILE__) . '/../src/TLogger.php';
 
-$log   = KLogger::instance(dirname(__FILE__), KLogger::DEBUG);
+$log   = TLogger::instance(dirname(__FILE__), TLogger::DEBUG);
 $args1 = array('a' => array('b' => 'c'), 'd');
 $args2 = NULL;
 
-$log->logInfo('Info Test');
-$log->logNotice('Notice Test');
-$log->logWarn('Warn Test');
-$log->logError('Error Test');
-$log->logFatal('Fatal Test');
-$log->logAlert('Alert Test');
-$log->logCrit('Crit test');
-$log->logEmerg('Emerg Test');
+$log->info('Info Test');
+$log->warn('Warn Test');
+$log->error('Error Test');
+$log->fatal('Fatal Test');
 
-$log->logInfo('Testing passing an array or object', $args1);
-$log->logWarn('Testing passing a NULL value', $args2);
+$log->info('Testing passing an array or object', $args1);
+$log->warn('Testing passing a NULL value', $args2);
---
+++ test/TLoggerTests.php
@@ -0,0 +1,45 @@
+<?php
+// lets us execute the tests from any directory
+define('ROOT_DIR',realpath(dirname(__FILE__)."/../"));
+
+include_once(ROOT_DIR.'/src/KLogger.php');
+include_once(ROOT_DIR.'/lib/EnhanceTestFramework.php');
+
+class KLoggerTests extends \Enhance\TestFixture
+{
+	private $log = null;
+
+	public function setUp()
+	{
+		$this->log = new KLogger('klogger-tests.log');
+	}
+
+	public function tearDown()
+	{
+		unlink($this->log->getLogFilePath());
+	}
+
+	public function smokeTest()
+	{
+		$this->log->debug("I'm a debug statement");
+		$this->log->info("I'm a info statement");
+		$this->log->warn("I'm a war statement");
+		$this->log->error("I'm a error statement");
+		$this->log->fatal("I'm a fatal statement");
+	}
+
+	public function smokeTestWithParams()
+	{
+		$debug = array("Luke"=>"Skywalker");
+		$this->log->debug("I'm a debug statement", $debug);
+		$this->log->info("I'm a info statement", $debug);
+		$this->log->warn("I'm a war statement", $debug);
+		$this->log->error("I'm a error statement", $debug);
+		$this->log->fatal("I'm a fatal statement", $debug);
+	}
+}
+
+// Run the tests
+\Enhance\Core::runTests();
+
+?>
